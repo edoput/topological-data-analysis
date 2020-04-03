c4215772e5d4473f7b80513c108436fc54cec064
Edoardo Putti <edoardo.putti@gmail.com>
2020-04-03 09:47:47 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='ripser-util',
+      version='0.1',
+      description='utilities to use ripser',
+      author='Edoardo Putti',
+      author_email='edoardo.putti@gmail.com',
+      url='https://github.com/edoput/topological-data-analysis/presentation',
+      packages=[
+        'presentation',
+      ]
+     )
+
